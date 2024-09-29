0dac804f3f558eba1c088fa4a9af9b506297756b
Alireza Sivariz <sivariz_a@cyberno.ir>
2024-09-29 11:37:24 +0330

    update :"Added new translations, updated quarantined overview page with buttons, and refactored access level management page with PageBox component"

--- src/pages/activity/quarantined/overview/index.tsx
+++ src/pages/activity/quarantined/overview/index.tsx
@@ -2,6 +2,7 @@ import { CustomDataGrid } from '@/components';
 import PageBox from '@/components/common/PageBox';
 import { quarantined_files } from '@/constants/tableHeaders';
 import useApi from '@/hooks/UseApi';
+import { useRouter } from 'next/router';
 
 interface QuarantinedData {
   [key: string]: any;
@@ -12,15 +13,40 @@ interface FetchDataResponse {
 }
 const QuarantinedOverview = () => {
   const { data, total, loading } = useApi<FetchDataResponse>('/agents/quarantined-files/');
+  const router = useRouter();
 
   return (
-    <PageBox searchQuery={quarantined_files} title="قرنطینه شده ها"
-    >
+    <PageBox searchQuery={quarantined_files} title="قرنطینه شده ها">
       <CustomDataGrid
         loading={loading}
         pageTotal={total}
         columns={quarantined_files}
         rows={data?.Data ?? []}
+        buttons={[
+          {
+            label: 'نتایج ضد ویروس مرکب',
+            type: 'extra',
+            onClick: () => {
+              router.push({
+                pathname: '/intelligence/antivirusResults',
+              });
+            },
+          },
+          {
+            label: 'دانلود فایل',
+            type: 'extra',
+            // onClick: (_, data) => {
+            //   router.push({
+            //     pathname: `/hosts/hostManagement/logs/detailsLog/`,
+            //     query: {
+            //       logId: data?.extra_information?.agent_id,
+            //       key: 'quarantined_files',
+            //       logs: 'alerts',
+            //     },
+            //   });
+            // },
+          },
+        ]}
       />
     </PageBox>
   );
