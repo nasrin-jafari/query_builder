f3b7adf3c974f4ede9130ab1576c63cdc4d9bff4
Alireza Sivariz <sivariz_a@cyberno.ir>
2024-10-06 16:24:32 +0330

    test

--- src/pages/hosts/hostManagement/logs/detailsLog/index.tsx
+++ src/pages/hosts/hostManagement/logs/detailsLog/index.tsx
@@ -1,52 +1,48 @@
-import { CustomDataGrid } from '@/components';
-import PageBox from '@/components/common/PageBox';
-import headers from '@/constants/tableHeaders';
-import useApi from '@/hooks/UseApi';
-import { useRouter } from 'next/router';
+// import { CustomDataGrid } from '@/components';
+// import PageBox from '@/components/common/PageBox';
+// import headers from '@/constants/tableHeaders';
+// import useApi from '@/hooks/UseApi';
+// import { useRouter } from 'next/router';
 import React from 'react';
 
-interface LogData {
-  Title: {
-    fa: string;
-    en: string;
-  };
-  Data: Array<Record<string, any>>;
-}
+// interface LogData {
+//   Title: {
+//     fa: string;
+//     en: string;
+//   };
+//   Data: Array<Record<string, any>>;
+// }
 
-interface UseApiResponse {
-  data: LogData | null;
-  total: number;
-  loading: boolean;
-}
+// interface UseApiResponse {
+//   data: LogData | null;
+//   total: number;
+//   loading: boolean;
+// }
 
 const DetailsLog: React.FC = () => {
-  const router = useRouter();
-  const { key, logId, logs } = router.query;
+  // const router = useRouter();
+  // const { key, logId, logs } = router.query;
 
-  const { data, total, loading }: UseApiResponse = useApi(`/agents/${logId}/logs/${logs}_${key}/`);
-  // Safely access the columns if data and Title.en exist
-  const columns = data?.Title?.en ? headers[data.Title.en] : [];
-
-  console.log(columns);
-  console.log(key, logId, logs);
+  // const { data, total, loading }: UseApiResponse = useApi(`/agents/${logId}/logs/${logs}_${key}/`);
+  // // Safely access the columns if data and Title.en exist
+  // const columns = data?.Title?.en ? headers[data.Title.en] : [];
   return (
     <>
-      {columns.length > 0 ? (
+      {/* {columns.length > 0 && (
         <PageBox
           title={data?.Title?.fa ?? ' '}
           description="توضیحات تکمیلی برای راهنمایی یا معرفی بخش بالا"
-          searchQuery={[]}
+          searchQuery={columns}
         >
           <CustomDataGrid
             loading={loading}
             pageTotal={total}
-            columns={[]}
+            columns={columns}
             rows={data?.Data ?? []}
           />
         </PageBox>
-      ) : (
-        'no'
-      )}
+      )} */}
+      <h1>ssss</h1>
     </>
   );
 };
