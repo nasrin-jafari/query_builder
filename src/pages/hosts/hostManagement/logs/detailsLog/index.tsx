66d1bb0aadb184ef304f007b9a403425caa5beac
Alireza Sivariz <sivariz_a@cyberno.ir>
2024-10-07 09:33:36 +0330

    test

--- src/pages/hosts/hostManagement/logs/detailsLog/index.tsx
+++ src/pages/hosts/hostManagement/logs/detailsLog/index.tsx
@@ -1,38 +1,42 @@
-import { CustomDataGrid } from '@/components';
-import headers from '@/constants/tableHeaders';
-import useApi from '@/hooks/UseApi';
+// import { CustomDataGrid } from '@/components';
+// import headers from '@/constants/tableHeaders';
+// import useApi from '@/hooks/UseApi';
+// import { useRouter } from 'next/router';
+import PageBox from '@/components/common/PageBox';
 import { Box } from '@mui/material';
-import { useRouter } from 'next/router';
 import React from 'react';
 
-interface LogData {
-  Title: {
-    fa: string;
-    en: string;
-  };
-  Data: Array<Record<string, any>>;
-}
+// interface LogData {
+//   Title: {
+//     fa: string;
+//     en: string;
+//   };
+//   Data: Array<Record<string, any>>;
+// }
 
-interface UseApiResponse {
-  data: LogData | null;
-  total: number;
-  loading: boolean;
-}
+// interface UseApiResponse {
+//   data: LogData | null;
+//   total: number;
+//   loading: boolean;
+// }
 
 const DetailsLog: React.FC = () => {
-  const router = useRouter();
-  const { key, logId, logs } = router.query;
+  // const router = useRouter();
+  // const { key, logId, logs } = router.query;
 
-  const { data, total, loading }: UseApiResponse = useApi(`/agents/${logId}/logs/${logs}_${key}/`);
-  const columns = data?.Title?.en ? headers[data.Title.en] : [];
-  console.log(data, 'data');
-  console.log(total, 'total');
-  console.log(loading, 'loading');
-  console.log(columns, 'col');
+  // const { data, total, loading }: UseApiResponse = useApi(`/agents/${logId}/logs/${logs}_${key}/`);
+  // const columns = data?.Title?.en ? headers[data.Title.en] : [];
+  // console.log(data, 'data');
+  // console.log(total, 'total');
+  // console.log(loading, 'loading');
+  // console.log(columns, 'col');
 
   return (
     <Box>
-      <CustomDataGrid loading={false} pageTotal={2} columns={[]} rows={[]} notExtra />
+      {/* <CustomDataGrid loading={false} pageTotal={2} columns={[]} rows={[]} notExtra /> */}
+      <PageBox title="asdsad">
+        <h1></h1>
+      </PageBox>
     </Box>
   );
 };
