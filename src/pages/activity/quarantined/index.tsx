25edaddc971ed405fd5a2009bc6240c433dcfd3d
Alireza Sivariz <sivariz_a@cyberno.ir>
2024-10-07 08:15:35 +0330

    change quarantined data fetch

--- src/pages/activity/quarantined/index.tsx
+++ src/pages/activity/quarantined/index.tsx
@@ -1,7 +1,7 @@
 import { CustomDataGrid } from '@/components';
+import PageBox from '@/components/common/PageBox';
 import headers from '@/constants/tableHeaders';
 import useApi from '@/hooks/UseApi';
-import { Box } from '@mui/material';
 import { useRouter } from 'next/router';
 import React from 'react';
 
@@ -24,16 +24,25 @@ const DetailsLog: React.FC = () => {
   const { key, logId, logs } = router.query;
 
   const { data, total, loading }: UseApiResponse = useApi(`/agents/${logId}/logs/${logs}_${key}/`);
+  // Safely access the columns if data and Title.en exist
   const columns = data?.Title?.en ? headers[data.Title.en] : [];
-  console.log(data, 'data');
-  console.log(total, 'total');
-  console.log(loading, 'loading');
-  console.log(columns, 'col');
-
   return (
-    <Box>
-      <CustomDataGrid loading={false} pageTotal={2} columns={[]} rows={[]} notExtra />
-    </Box>
+    <>
+      {columns.length > 0 && (
+        <PageBox
+          title={data?.Title?.fa ?? ' '}
+          description="توضیحات تکمیلی برای راهنمایی یا معرفی بخش بالا"
+          searchQuery={columns}
+        >
+          <CustomDataGrid
+            loading={loading}
+            pageTotal={total}
+            columns={columns}
+            rows={data?.Data ?? []}
+          />
+        </PageBox>
+      )}
+    </>
   );
 };
 
